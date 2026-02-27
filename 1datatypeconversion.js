cc70bd4156f5c6254eadc2aa76f337d8692ad760
Vishgit212 <vishgit212@gmail.com>
2026-02-27 22:21:31 +0530

    q

---
+++ 1datatypeconversion.js
@@ -0,0 +1,45 @@
+/*let score = "33 "
+
+console.log(typeof score) // string
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber) // number
+console.log(valueInNumber)
+
+ 
+let score = "33abch "  
+
+console.log(typeof score) // string
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber) // number
+console.log(valueInNumber) // nan  = not a number
+
+
+null
+let score = " null"  
+
+console.log(typeof score) // string
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber) // number
+console.log(valueInNumber) // 0 
+
+*/
+
+// " 33 "  => 33
+// " 33abch " => NaN 
+// " null" => 0 
+// true => 1
+// false => 0
+
+
+let isloggedIn = "1"
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn) // true
+
+
+
+
+
