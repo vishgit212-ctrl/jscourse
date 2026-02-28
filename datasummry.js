a93eab3d84fafad0ab6657788a104fbee9ff2071
Vishgit212 <vishgit212@gmail.com>
2026-02-28 12:26:26 +0530

    datasumary1

---
+++ datasummry.js
@@ -0,0 +1,38 @@
+// primitive data type  7 
+// 7 type : string ,number , bolean , null , undefined , symbol  , bigint
+
+// JavaScript (JS) is a dynamically typed language
+  
+const id = Symbol( "123")
+const anotherid= Symbol( "123")
+//console.log(id === anotherid)
+
+
+
+
+
+
+//  refrence ( non primitive)
+
+// array , object , function 
+
+const heros = [ "shakti man " , " naagraj," , " bheeem "]
+const myobj = { 
+         name : " hitesh ",
+        age : 22, 
+        
+}
+
+const myfun= function (){
+     console.log("hello world ")
+
+}
+
+console.log(typeof(id) )
+console.log(typeof(myfun) ) // function function hi return karta ha jisko bola jata ha object function 
+
+
+myfun()
+
+
+
