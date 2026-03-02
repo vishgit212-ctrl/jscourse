1e315c86d65e806354d1c2f79341cd09036517c3
Vishgit212 <vishgit212@gmail.com>
2026-03-02 13:07:58 +0530

    string2

--- string.js
+++ string.js
@@ -3,8 +3,14 @@ const repcoutn = 50
 
 // console .log ( name + repcoutn + " value   " )
 
-console.log ( `hello my name is  $ { name} and my repo count is ${repcoun}`) 
+//console.log ( `hello my name is  $ { name} and my repo count is ${repcoun}`) 
 // bactics thi is string inter polation  
 
-
+ const new1 = new String("Vishhh")
+// console.log(typeof(name))
+// console.log(typeof(new1))
+// console.log(new1.toUpperCase()) // convert uppercase 
+//console.log(new1.length)// find length 
+console.log(new1.charAt(2)) // we put index number we find the we find the value of this index number 
+console.log(new1.indexOf('s'))
 
