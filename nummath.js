ec5143c4488e84c7dfd253b09e21d350b9f05524
Vishgit212 <vishgit212@gmail.com>
2026-03-02 15:53:30 +0530

    nummath1

---
+++ nummath.js
@@ -0,0 +1,31 @@
+const score = 400 
+const balance = new Number(8236487236487264)
+// console.log(balance)
+// console.log(score)
+
+console.log(balance.toString().length) 
+// console.log(balance.toFixed(2)) // tofixed is ley use kart ha ko ki agra pont ka bad bhot sari value ha tho shirf 2 hi value do muje 
+
+
+const othernumber = 123.937482
+// console.log(othernumber.toPrecision (3))// 
+
+const hundred= 100000000000 
+console.log(hundred.toLocaleString('en-IN'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
