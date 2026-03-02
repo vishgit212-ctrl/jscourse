0f97fcc352c0551d14853bb7bb26b0ad0fc3631d
Vishgit212 <vishgit212@gmail.com>
2026-03-02 11:46:00 +0530

    datasumm

--- datasummry.js
+++ datasummry.js
@@ -28,11 +28,42 @@ const myfun= function (){
 
 }
 
-console.log(typeof(id) )
-console.log(typeof(myfun) ) // function function hi return karta ha jisko bola jata ha object function 
-
-
-myfun()
+let anu   = null
+//console.log(typeof(anu) )// null return object 
 
 
 
+//console.log(typeof(id) )
+//console.log(typeof(myfun) ) // function function hi return karta ha jisko bola jata ha object function 
+// null bhi
+
+//myfun()
+
+
+
+//  two types off memory 
+//   stack ( primitive ), heap ( non primitive )
+
+let myyoutubename  = " chai code "
+let anothername = myyoutubename
+
+ anothername = " hiteshchodri"
+
+//console.log( myyoutubename)
+ //console.log(anothername)
+
+
+ let user1= { 
+ 
+     email : "hiti@gmail.com",
+     upi : " usr1"
+
+ }
+
+
+let user2= user1
+
+
+user2.email= " vish@gmail.com"
+console.log(user1.email)
+console.log(user2.email)
