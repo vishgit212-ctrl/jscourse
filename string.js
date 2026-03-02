851f4c67b799a49702a84f352b1cb1d7fbb59718
Vishgit212 <vishgit212@gmail.com>
2026-03-02 11:59:51 +0530

    string1

---
+++ string.js
@@ -0,0 +1,10 @@
+const name =" hitu "
+const repcoutn = 50 
+
+// console .log ( name + repcoutn + " value   " )
+
+console.log ( `hello my name is  $ { name} and my repo count is ${repcoun}`) 
+// bactics thi is string inter polation  
+
+
+
