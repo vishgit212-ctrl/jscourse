0e74915e7ca89f32d79827cb49b83c05519ebef1
Vishgit212 <vishgit212@gmail.com>
2026-03-03 18:12:42 +0530

    1

--- basicjs/nummath.js
+++ basicjs/nummath.js
@@ -1,35 +1,35 @@
-// const score = 400 
-// const balance = new Number(8236487236487264)
-//  console.log(balance)
-// console.log(score)
+const score = 400 
+const balance = new Number(8236487236487264)
+ console.log(balance)
+console.log(score)
 
-// console.log(balance.toString().length) 
-// console.log(balance.toFixed(2)) // tofixed is ley use kart ha ko ki agra pont ka bad bhot sari value ha tho shirf 2 hi value do muje 
-
-
-// const othernumber = 123.937482
-// console.log(othernumber.toPrecision (3))// 
-
-// const hundred= 100000000000 
-// console.log(hundred.toLocaleString('en-IN'))
-// // ---------------maths -------------------
+console.log(balance.toString().length) 
+console.log(balance.toFixed(2)) // tofixed is ley use kart ha ko ki agra pont ka bad bhot sari value ha tho shirf 2 hi value do muje 
 
 
+const othernumber = 123.937482
+console.log(othernumber.toPrecision (3))// 
+
+const hundred= 100000000000 
+console.log(hundred.toLocaleString('en-IN'))
+// ---------------maths -------------------
 
 
-// console.log(Math)
-// console.log(Math.abs(-50))
-//  console.log(Math.round(4.8))
-// console.log(Math.ceil(4.2))// ya 4 ka bad 5 hi output dega 
-// console.log(Math.floor(4.9))// ya 4 ka bad pont ma 5 hi kyo na ho ya 4 hi value dega 
-
-// console.log(Math.min(4,5,6,7,8,54,1))
-// console.log(Math.max(4,5,6,7,8,54,1))
 
 
-//  console.log(Math.random())//  values always come from between 0 or 1 
+console.log(Math)
+console.log(Math.abs(-50))
+ console.log(Math.round(4.8))
+console.log(Math.ceil(4.2))// ya 4 ka bad 5 hi output dega 
+console.log(Math.floor(4.9))// ya 4 ka bad pont ma 5 hi kyo na ho ya 4 hi value dega 
 
-//  console.log((Math.random()*10)+1)
+console.log(Math.min(4,5,6,7,8,54,1))
+console.log(Math.max(4,5,6,7,8,54,1))
+
+
+ console.log(Math.random())//  values always come from between 0 or 1 
+
+ console.log((Math.random()*10)+1)
 
 const min= 10 
 const max= 20
