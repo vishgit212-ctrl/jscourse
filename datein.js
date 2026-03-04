b767427acad3ec2a4daf5084ae5917d9da4c0a78
Vishgit212 <vishgit212@gmail.com>
2026-03-04 09:46:50 +0530

    datein2

---
+++ datein.js
@@ -0,0 +1,16 @@
+ // dates
+
+
+ let mydate = new Date()
+
+
+//   console.log(mydate.toString())
+//    console.log(mydate.toDateString())
+//       console.log(mydate.toLocaleString())
+//       console.log(typeof mydate)
+
+
+
+let mycreatedate = new Date() // javascript ma month zero sa start hota haa 
+console.log(mycreatedate.toDateString())
+
