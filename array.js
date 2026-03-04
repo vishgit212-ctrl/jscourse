219641ed6083640d7bee9acbffb9cf06cc42295d
Vishgit212 <vishgit212@gmail.com>
2026-03-04 17:54:01 +0530

    arraycomit

---
+++ array.js
@@ -0,0 +1,22 @@
+// array is object
+
+// in javascript arry is resizable 
+//JavaScript arrays are resizable and can contain a mix of different data types.
+// javascript follow the shallow copy it give same refrence of the memory 
+
+const myarray = [1, 2, 3, 4, 5 ] ;
+// const myheros= [" shaktiman", "naagraj"]
+ 
+// console.log(myArray[4])// we print the value like this 
+
+
+// console.log(typeof(myheros))
+
+
+// array methods
+
+  myarray.push(6)// it add the value 
+
+console.log(myarray)
+
+
