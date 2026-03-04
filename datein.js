72330f6edbd5d953b5283f5ea661ce1f75e1a052
Vishgit212 <vishgit212@gmail.com>
2026-03-04 17:21:42 +0530

    date4

--- datein.js
+++ datein.js
@@ -15,13 +15,29 @@
 // console.log(mycreatedate.toDateString())
 
 
-// let mycreatedate = new Date("2025-03-4")
+ let mycreatedate = new Date("2025-03-4")
 //let mycreatedate = new Date("03-04-2026")
 //console.log(mycreatedate.toLocaleString())
 
 let mytimestamp = Date.now()
-console.log(mytimestamp)
+// console.log(mytimestamp)
+
+//console.log(mycreatedate.getTime())
+
+// console.log(Math.floor(Date.now()/1000)) // 
+
+let newdate = new Date()
+// console.log(newdate)// it  give you date 
+// console.log(newdate.getMonth()+1)
+
+// console.log(newdate.getDay()+1)
 
 
+///////////////////////
+// very important
+
+newdate.toLocaleString('default',{
+   weekday:"long",
 
 
+})
