1829b7f76a5ae43bd68699f8e3c005a4759a1613
Vishgit212 <vishgit212@gmail.com>
2026-03-07 17:54:28 +0530

    obj2

--- obj2.js
+++ obj2.js
@@ -36,7 +36,62 @@ const obj1 = { 1: "a", 2:"b" , 3:"d"}
 const obj2 = { 10: "a", 12:"b" , 63:"d"}
 
 //const obj3 = { obj1 ,obj2} // asa karne sa object ka ander object ajata ha 
-const obj3 = Object.assign(obj1, obj2)
-console.log(obj3)
+//const obj3 = Object.assign(obj1, obj2)
+
+const obj3 = {...obj1,...obj2}// always use spread operator 
+
+
+// console.log(obj3)
+
+//  array ka object 
+
+const user = [
+     {
+    id : 1 ,
+    email : " shsfshsf@gmail.com"
+},
+
+ {
+    id : 1 ,
+    email : " shsfshsf@gmail.com"
+},
+
+ {
+    id : 1 ,
+    email : " shsfshsf@gmail.com"
+},
+
+ {
+    id : 1 ,
+    email : " shsfshsf@gmail.com"
+}
+]
+
+// console.log(Object.keys(tinderuser))// use  key function to find the key of a object and it return in a array
+// console.log(Object.values(tinderuser))// use values fun to find the value 
+// console.log(Object.entries(tinderuser))// this function create every key value player in  arrya 
+
+// console.log(tinderuser.hasOwnProperty("name"))// this function check the property is exist or not and return boolean value
+
+// de-structure arrya ki bhi hoti ha or object ki bhi
+
+
+const course  = {
+    coursename  : " js in hindi ", 
+    price : " 9999", 
+    courseinstructor : " hitesh"
+
+}
+
+const {courseinstructor } = course // this is called de-structure of object and we can use this variable in our code
+
+console.log(courseinstructor)
+
+
+
+
+
+
+
 
 
