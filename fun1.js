d3424e737388f682f657441c06eae0c393e683a6
Vishgit212 <vishgit212@gmail.com>
2026-03-12 18:35:34 +0530

    fun1

--- fun1.js
+++ fun1.js
@@ -42,6 +42,77 @@ function loginuser(username = "sam"){
 const  name1 = loginuser("vishnu")
 //console.log(name1) //
 //console.log(loginuser())// if user does not pass somthing it return undefined 
-console.log(loginuser())
+//console.log(loginuser())
+
+function calculatecart (... num ){ // ... is a rest operator 
+          // spread operator return a  array 
+     return num
+
+}
+
+
+
+// console.log(calculatecart(232,323,333,))
+
+
+// function calculatecartprice( ...num1){ // spread operator 
+//      // ... this take multiple value in sigle time and  operator return in a  array 
+//      return num1
+
+
+// }
+
+function calculatecartprice(val1, val2,  ...num1){ // spread operator 
+     // val1 or val2 ka ander first 2 value pass hogi and the rest num1 rest operator array ma creat kar dega 
+
+     return num1
+
+
+} 
+
+// console.log(calculatecartprice(34 , 400 ,892 ,324234,234234,234234,2423 ))
+ 
+
+
+ const user ={ // object in function ma type safety check karni padti ha 
+
+
+     username : "vishsh",
+     price:2322
+ 
+ }
+
+
+
+ function handelobject(anyobject){
+     console.log(` username is ${anyobject.username} and price is ${ anyobject.price} `)
+
+ }
+
+
+ //handelobject(user)
+// handelobject({ // we can pass like this also 
+//  username:"dsfsdfsd",
+//  price: 64646
+
+// })
+const newarry=[ 34234,234234,234234,234234,4554]
+
+function returnsecvalue (getarray){
+
+     return getarray[3]
+
+
+}
+
+
+// console.log(returnsecvalue(newarry))
+
+console.log(returnsecvalue([344,2323,2,2,5332,222]))
+
+
+
+
+
 
 
