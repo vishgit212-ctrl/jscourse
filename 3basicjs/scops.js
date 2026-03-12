1dbb2d6933653953fbad7c230d0932576c18ee98
Vishgit212 <vishgit212@gmail.com>
2026-03-12 19:20:19 +0530

    scops

---
+++ 3basicjs/scops.js
@@ -0,0 +1,55 @@
+
+function one ()
+{
+     const username = "vishshshs"
+
+    function two(){
+
+            const website = " youtube "
+            console.log( username)
+
+    }
+     //console.log(website)
+
+ two()
+}
+
+// one()
+
+
+// if (true){
+
+//     const username = "vish"
+
+// if (username === "vish"){
+//     const website = " youtube"
+
+//     console.log(username + website)
+
+// }
+//  console.log(website)
+
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+function addone(num){
+
+    return num +1
+
+
+}
+
+
+addone(4) //this function we cam call any where in this file 
+
+
+
+const addtwo = function (num ){ //  addtwo now variable but we also called expresion 
+    return num +2 
+
+}
+
+addtwo()// is trah function declear karne sa hum usko shirf function ko uska nicha hi call kar sakte ha 
+
