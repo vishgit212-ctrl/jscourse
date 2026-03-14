0a44441e086dec7bb0d3750a076e1c827f566b1a
Vishgit212 <vishgit212@gmail.com>
2026-03-14 17:51:21 +0530

    one

---
+++ one.js
@@ -0,0 +1,24 @@
+const isuserlogin = true 
+
+const temrature = 41
+
+// if (temrature < 50 ){
+
+//     console.log("less than 50 ")
+
+// }else {
+
+// console.log("temperature is grater than 50 ")
+
+// }
+
+
+// <, > , <=  , >=  , == , === , !=
+
+
+
+  
+
+
+
+ 
