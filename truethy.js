3f6fb7660da8706fb7b4196ae34415c6a3f8948d
Vishgit212 <vishgit212@gmail.com>
2026-03-15 08:17:51 +0530

    trythy

---
+++ truethy.js
@@ -0,0 +1,61 @@
+//const useremail = "vish@gmail.com"
+//const useremail = ""// false 
+const useremail = [] // empty array true hota ha 
+if (useremail){
+
+    console.log("got user email")
+}
+else{
+
+    console.log("dony have user ")
+}
+
+
+// false value 
+
+//false , 0 , "", -0,bigint 0n , null , undefined  , nan
+
+// baki sab true 
+// "0", " false ", " ", [], {}, function()
+
+
+// const array =[]
+
+// if (array.lenght===0){
+
+//     console.log( " empty array")
+
+// }
+
+// const  obj = {}
+// if ( Object.key(obj).lenght === 0)
+// {
+//     console.log(" onj is empty  ")
+
+
+// }
+
+
+
+// nullish coalescing operator (??) : null undefined 
+
+let vall ;
+// vall = 4 ?? 50
+// vall = null ?? 78
+
+// var1 = undefined ?? 29
+
+// val1 = null  ?? 29 ?? 67 // asa case ka null ka bad jo  bhi value hogi wahi asign hogi 
+
+
+// console.log(var1)
+
+// terniary operator  
+
+// condition ? true : false
+
+
+
+const iceteaprice = 100
+
+iceteaprice >= 80 ? console.log("less than 80") : console.log("more than 80 ")
