2c95dc3927305592be822bc913ba7810b03ff08b
Vishgit212 <vishgit212@gmail.com>
2026-03-15 09:04:13 +0530

    iteration

---
+++ iteration/one.js
@@ -0,0 +1,73 @@
+// for 
+
+
+for ( let index= 0 ; index <= 27 ; index ++  ){
+
+        const element =  index 
+      //  console.log(element)   
+
+
+}
+
+for  ( i = 3 ; i <= 3 ; i ++  ){
+
+    //console.log(` outer loop value : ${i}`)
+
+    for ( j = 0 ; j <= 10 ; j ++ ){
+
+       // console.log(i + '*' + j + " = " + i * j )
+       // console.log(` inner loop value ${j} and inner loop ${i} `)
+    }
+
+}
+
+
+const   myarray =  [ " flash ",  "  bataman ", " superman "]
+
+//console.log(myarray.length)
+for ( let index = 0 ; index < myarray.length  ;  index ++ ){ 
+
+    const element  = myarray [ index ] 
+   // console.log( element)
+
+
+}
+
+//  keywords 
+// break and continue 
+
+
+// for (let index = 1 ; index <= 20; index ++ ){
+
+
+//     if (index  == 5  ){
+
+//         console .log ( ' dedected  5 ')
+
+//         break 
+//     }
+
+
+//     console.log (`value of i is ${index}`)
+
+
+// }
+for (let index = 1 ; index <= 20; index ++ ){
+
+
+    if (index  == 5  ){
+
+        console .log ( ' dedected  5 ')
+
+       continue 
+    }
+
+
+    console.log (`value of i is ${index}`)
+
+
+}
+
+
+ 
+
